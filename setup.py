284dd19c8ee20e4a68749f83ef7dfb2386fa9532
wray <wray@wrayesian.com>
2016-10-29 20:14:15 -0400

    adding setup for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'create_em',
+  packages = ['create_em'],
+  version = '0.1',
+  description = 'Python Code for Tech Em Studios Create Em Class',
+  author = 'Techem Studios',
+  author_email = 'wray@techemstudios.com',
+  url = 'https://github.com/wray/create-em',
+  download_url = 'https://github.com/peterldowns/create-em',
+  keywords = ['slackbot', 'RPi', 'AWS'],
+  classifiers = [],
+)
