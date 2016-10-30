d3cd66632dcbdefa30bff25cc656582fbe88b6a9
wray <wray@wrayesian.com>
2016-10-30 15:34:56 -0400

    c'mon Travis!!

--- slackbot_ce/wray/temp_humidity.py
+++ slackbot_ce/wray/temp_humidity.py
@@ -1,11 +1,14 @@
 import os
 import time
+
+sensor = None
+
 try:
     import Adafruit_DHT
+    sensor = Adafruit_DHT.DHT22
 except ImportError:
     pass
 
-sensor = Adafruit_DHT.DHT22
 pin = 4
 
 def read_temp_humidity():
