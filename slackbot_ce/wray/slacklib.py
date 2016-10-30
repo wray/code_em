78755ac06a9efac51a08c25c5b3057db5b0f8b95
wray <wray@wrayesian.com>
2016-10-30 15:26:15 -0400

    trying dht - may not be able to install package

--- slackbot_ce/wray/slacklib.py
+++ slackbot_ce/wray/slacklib.py
@@ -2,6 +2,8 @@ import random
 import json
 import urllib3
 
+import temp_humidity
+
 COMMAND1 = "who are you"
 COMMAND2 = "what can you do"
 COMMAND3 = "temp"
@@ -18,7 +20,8 @@ def handle_command(command):
     elif command.find(COMMAND2) >= 0:
         response = "Not much right now... waiting for students to teach me."
     elif command.find(COMMAND3) >= 0:
-        response = "Building up skills to sense temperature and humidity now. Stay tuned."
+        temp_c,temp,humidity = temp_humidity.read_temp_humidity()
+        response = "At my location, the temperature is " + str(temp) + " and the relative humidity is " + str(humidity)
     elif command.find(COMMAND4) >= 0:
         http = urllib3.PoolManager()
         animals = json.loads(http.request('GET','https://www.randomlists.com/data/animals.json').data.decode('utf-8'))['data']
