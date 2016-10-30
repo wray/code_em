f6d720369fb3156b131a485ce0330a23a03f5711
wray <wray@wrayesian.com>
2016-10-30 08:05:55 -0400

    setting up release on every passing build

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
+import time
+
 from setuptools import find_packages
 
 from distutils.core import setup
+
+patch_level = int(time.time())
+
+ver = "0.1." + str(patch_level)
+
 setup(
   name = 'slackbot_ce',
   packages = find_packages(),
-  version = '0.1',
+  version = ver,
   description = 'Python Code for Tech Em Studios Create Em Class',
   author = 'Techem Studios',
   author_email = 'wray@techemstudios.com',
   url = 'https://github.com/wray/create_em',
-  download_url = 'https://github.com/wray/create_em/tarball/0.1',
+  download_url = 'https://github.com/wray/create_em/tarball/'+ver,
   keywords = ['slackbot', 'RPi', 'AWS'],
   classifiers = [],
 )
