b8bfc183d12cb57ac55b0f2b3476a2b68c8f828d
wray <wray@wrayesian.com>
2016-10-30 12:24:19 -0400

    added another command

--- slackbot_ce/wray/slacklib.py
+++ slackbot_ce/wray/slacklib.py
@@ -1,6 +1,7 @@
 
 COMMAND1 = "who are you"
 COMMAND2 = "what can you do"
+COMMAND3 = "temp"
 
 def handle_command(command):
     """
@@ -12,6 +13,8 @@ def handle_command(command):
         response = "I am a simpleton bot."
     elif command.find(COMMAND2) >= 0:
         response = "Not much right now... waiting for students to teach me."
-
+    elif command.find(COMMAND3) >= 0:
+        response = "Building up skills to sense temperature and humidity now. Stay tuned."
+        
     return response
 
