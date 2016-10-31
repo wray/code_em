0b04e6ffcd089ac191cb4e5fd5c6bec412399236
joe <joe@techemstudios.com>
2016-10-31 15:50:47 -0400

    added command1

--- slackbot_ce/joe/slacklib.py
+++ slackbot_ce/joe/slacklib.py
@@ -1,5 +1,7 @@
+
+
 # Put your commands here
-COMMAND1 = ""
+COMMAND1 = "Are you awake?"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -9,7 +11,7 @@ def handle_command(command):
     """
     response = ""
     if command.find(COMMAND1) >= 0:
-        response = "Huh?"
+        response = "Just barely..."
         
     return response
 
