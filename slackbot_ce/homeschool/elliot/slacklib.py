60b3144d17554d1152451187e9eda80ee5a44a65
jo3t3ch3m <te-admin@wrayesian-1.local>
2016-11-08 13:36:45 -0500

    Elliott makes command 2.
    command 2

--- slackbot_ce/homeschool/elliot/slacklib.py
+++ slackbot_ce/homeschool/elliot/slacklib.py
@@ -1,6 +1,10 @@
 # Put your commands here
 COMMAND1 = "what do i look like?"
+<<<<<<< Updated upstream
 COMMAND2 = ""
+=======
+COMMAND2 = "bluedibal"
+>>>>>>> Stashed changes
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -13,6 +17,9 @@ def handle_command(command):
         response = "does it look like i have eyes too you."
     elif command.find(COMMAND2) >= 0:
           response = ""
+        
+    elif command.find(COMMAND2) >= 0:
+        response = "eniglesh plz"
         
     return response
 
