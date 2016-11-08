d9b8d0760e24f6c3f67d6e107e7fc6e0f26a9297
wray <wray@wrayesian.com>
2016-11-08 12:15:10 -0500

    removing new command methods

--- slackbot_ce/wray/slacklib.py
+++ slackbot_ce/wray/slacklib.py
@@ -10,8 +10,6 @@ COMMAND1 = "who are you"
 COMMAND2 = "what can you do"
 COMMAND3 = "temp"
 COMMAND4 = "name an animal"
-COMMAND5 = "new command"
-COMMAND6 = "del command"
 
 def handle_command(command):
     """
@@ -35,35 +33,6 @@ def handle_command(command):
         animals = json.loads(http.request('GET','https://www.randomlists.com/data/animals.json').data.decode('utf-8'))['data']
         response = animals[random.randint(0,len(animals)-1)]
 
-    elif command.find(COMMAND5) >= 0:
-        command = command.encode('utf-8')
-        commands = shelve.open('commands')
-        parse = command.split(',')
-        pc = parse[0].find(':')
-        parse_command = parse[0][pc+1:].strip()
-        commands[parse_command] = parse[1].strip()
-        commands.sync()
-        print commands
-        commands.close()
-        response = "ok"
-
-    elif command.find(COMMAND6) >= 0:
-        command = command.encode('utf-8')
-        commands = shelve.open('commands')
-        pc = command.find(':')
-        try:
-            commands.pop(command[pc+1:].strip())
-        except:
-            pass
-        commands.sync()
-        commands.close()
-        response = "ok"
-    else:
-        command = command.encode('utf-8')
-        commands = shelve.open('commands')
-        if commands.has_key(command.strip()):
-            response = commands[command.strip()]
-        commands.close()
         
     return response
 
