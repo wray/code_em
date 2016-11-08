72445407ca2aa90c8bd6c672581a687b2156d5a0
joe@techemstudios.com <joe@techemstudios.com>
2016-11-08 12:50:26 -0500

    COMMAND2

--- slackbot_ce/joe/slacklib.py
+++ slackbot_ce/joe/slacklib.py
@@ -5,6 +5,7 @@ COMMAND1 = "are you awake?"
 COMMAND2 = "what is your full name?"
 COMMAND3 = "what is your favorite snack?"
 COMMAND4 = "green led"
+COMMAND5 = "how do you clean your hands?"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -33,6 +34,8 @@ def handle_command(command):
             response = "ok"
         else:
             response = "I'm not sure what to do with the green led."
+    elif command.find(COMMAND5) >= 0:
+        response = "well, this is embarassing... I don't have hands."
         
     return response
 
