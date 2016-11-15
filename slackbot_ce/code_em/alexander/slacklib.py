c9b621262c2f39b26320620d58ba0c2dddd7ae2f
Bogzworth <trolcomputer@gmail.com>
2016-11-15 18:44:29 -0500

    Update slacklib.py

--- slackbot_ce/code_em/alexander/slacklib.py
+++ slackbot_ce/code_em/alexander/slacklib.py
@@ -1,15 +1,15 @@
 # Put your commands here
-COMMAND1 = ""
-
+COMMAND1 = "what is the best video "
+COMMAND2 = ""
 # Your handling code goes in this function
 def handle_command(command):
     """
         Determine if the command is valid. If so, take action and return
         a response, if necessary.
     """
-    response = ""
+    response = "no"
     if command.find(COMMAND1) >= 0:
-        response = "Huh?"
+        response = "Castelvania"
         
     return response
 
