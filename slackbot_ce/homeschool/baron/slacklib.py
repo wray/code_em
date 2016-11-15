8afadd0fc033acdd192e0773b2af9321d5b836a6
GammingBaron <baronwright@gmail.com>
2016-11-15 13:20:49 -0500

    COMMAND 4 5 and 6

--- slackbot_ce/homeschool/baron/slacklib.py
+++ slackbot_ce/homeschool/baron/slacklib.py
@@ -2,6 +2,9 @@
 COMMAND1 = "what is you harry potter house?"
 COMMAND2 = "1 times 8,888"
 COMMAND3 = "what is your favrit tree?"
+COMMAND4 = "hello world"
+COMMAND5 = "space unicorn"
+COMMAND6 = "what is the couler of the sky"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -18,6 +21,15 @@ def handle_command(command):
 
     elif command.find(COMMAND3) >= 0:
          response = "Pine tree"
+
+    elif command.find(COMMAND4) >= 0:
+           response = "its me"
+
+    elif command.find (COMMAND5) >= 0:
+            response = "soring thru the sky delivering the rainbow all a round the world"
+
+     elif command.find (COMMAND6) >= 0:
+            response = "i dont have eyes" 
        
       
         
