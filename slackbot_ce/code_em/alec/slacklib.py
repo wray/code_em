b19f8341deedc603441183bb1c50acd1df687c27
Alec <cachalfant@Comcast.net>
2016-11-15 18:40:37 -0500

    Command 1 & 2 changed.

--- slackbot_ce/code_em/alec/slacklib.py
+++ slackbot_ce/code_em/alec/slacklib.py
@@ -1,7 +1,20 @@
 # Put your commands here
-COMMAND1 = ""
+COMMAND1 = "Hi"
+COMMAND2 = "ERROR..."
 
-# Your handling code goes in this function
+#Your handling code toes in this function
+def handle_command(comand):	
+	response = ""
+	if command.find(COMMAND1) >= 0:
+		response="Donald J. Trump"
+	elif command.find(COMMAND2) >= 0:
+		response="Donald will WIN"
+	return response
+while True:
+	command = raw_input('serexona > ')
+	response = handle_command(command)
+	print response
+	print	Your handling code goes in this function
 def handle_command(command):
     """
         Determine if the command is valid. If so, take action and return
@@ -9,7 +22,11 @@ def handle_command(command):
     """
     response = ""
     if command.find(COMMAND1) >= 0:
-        response = "Huh?"
+        response = "Oh hi there, Please input ERROR..."
+
+    elif command.find(COMMAND2) >= 0:
+        for i in range(100000000000000000000000000000):
+            response = "ERROR..."
         
     return response
 
