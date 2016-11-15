a949643722f5710575ab9cf7c05d325c4e6b12ea
Evelyn Fite <baronwright@gmail.com>
2016-11-15 18:42:29 -0500

    I added three commands. 😄

--- slackbot_ce/code_em/evelyn/slacklib.py
+++ slackbot_ce/code_em/evelyn/slacklib.py
@@ -1,5 +1,8 @@
 # Put your commands here
-COMMAND1 = ""
+COMMAND1 = "whats your name?"
+COMMAND2 = "What is mary poppins??"
+COMMAND3 = "what is pie?"
+COMMAND4 = "why are you a pie?"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -9,7 +12,14 @@ def handle_command(command):
     """
     response = ""
     if command.find(COMMAND1) >= 0:
-        response = "Huh?"
+        response = "Im Piebot!"
         
-    return response
+    elif command.find(COMMAND2) >= 0:
+        response = "Shes supercalifragulisticexpialidocious"
+    elif command.find(COMMAND3) >= 0:
+        resonse = "How could you not know this? Have you been living under a rock? Pie is a delicious pastry, of course!"
 
+
+    elif command.find(COMMAND4) >= 0:
+        response = "Because pie is awesome, and Im awesome, so I must be a pie!"
+     return response
