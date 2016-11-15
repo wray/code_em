2c4ee854f4702bf715320bc3eee8e151a36ab2e2
GammingBaron <baronwright@gmail.com>
2016-11-15 12:58:02 -0500

    Command 1 2 and 3

--- slackbot_ce/homeschool/baron/slacklib.py
+++ slackbot_ce/homeschool/baron/slacklib.py
@@ -1,7 +1,7 @@
 # Put your commands here
-(COMMAND1) What is you harry potter house?
-(COOMMAND2) 1 times 8,888
-(COMMAND3) What is your favrit tree?
+COMMAND1 "What is you harry potter house?"
+COMMAND2 "1 times 8,888"
+COMMAND3 "What is your favrit tree?"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -10,14 +10,14 @@ def handle_command(command):
         a response, if necessary.
     """
     response = ""
-    if command.find(COMMAND1) >= 0:
-        response = Greffendor 
+    if command.find"COMMAND1 >= 0:
+        response = "Greffendor"
           
-    if command.find(COMMAND2) >= 0:
-         response = 8,888
+    if command.find "COMMAND" >= 0:
+         response = "8,888"
 
-    if command.find (COOMAND3) >= 0:
-         response = Pine tree
+    if command.find "Comaand" >= 0:
+         response = "Pine tree"
        
       
         
