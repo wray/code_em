1a7402abc149ac371bb34f9870f348a7217a5300
Alex <bossmoss012@gmail.com>
2016-11-17 13:02:50 -0500

    Alex commit syntax fix

--- slackbot_ce/code_em/alex/slacklib.py
+++ slackbot_ce/code_em/alex/slacklib.py
@@ -1,5 +1,6 @@
 # Put your commands here
-COMMAND1 = ""
+COMMAND1 = "Damn Daniel"
+COMMAND2 = "Back at it again in the wrong hood"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -7,9 +8,12 @@ def handle_command(command):
         Determine if the command is valid. If so, take action and return
         a response, if necessary.
     """
-    response = ""
+    response = "Huh?"
     if command.find(COMMAND1) >= 0:
         response = "Huh?"
+
+    elif command.find(COMMAND2) >= 0:
+        response = "I'm not sure I follow."
         
     return response
 
