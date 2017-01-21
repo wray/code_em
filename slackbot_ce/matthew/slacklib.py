77023293f5f7e3291bbe6a245c54290d998aba1a
Matthew <aewozniak@gmail.com>
2017-01-21 13:05:02 -0500

    fix again - hop it works this time (ugh)

--- slackbot_ce/matthew/slacklib.py
+++ slackbot_ce/matthew/slacklib.py
@@ -1,6 +1,7 @@
 # Put your commands here
 COMMAND1 = "who is mr_techie"
 COMMAND2 = "talk in binary"
+COMMAND3 = "i dont like you"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -14,6 +15,6 @@ def handle_command(command):
     elif command.find(COMMAND2) >= 0:
         response = "10010010 01010101 01010000 11111101 01010110 01010111 01011110 10101010 10101001\n00010111 11110101 01010010!!!"
     elif command.find(COMMAND3) >= 0:
-        responce =
+        responce = "Well, I don't like you either!"
 
     return response
