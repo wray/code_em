ddc6cf9bfe77f28d485b4eb926450803a7aa32ed
Matthew <aewozniak@gmail.com>
2017-01-21 13:00:00 -0500

    fix my stuff

--- slackbot_ce/matthew/slacklib.py
+++ slackbot_ce/matthew/slacklib.py
@@ -11,7 +11,9 @@ def handle_command(command):
     response = ""
     if command.find(COMMAND1) >= 0:
         response = "I cannot reveal his identity."
-    if command.find(COMMAND2) >= 0:
+    elif command.find(COMMAND2) >= 0:
         response = "10010010 01010101 01010000 11111101 01010110 01010111 01011110 10101010 10101001\n00010111 11110101 01010010!!!"
+    elif command.find(COMMAND3) >= 0:
+        responce =
 
     return response
