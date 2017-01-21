ca23ad6b50c36f46a1ce5e9c16698b7c2aceba04
Matthew <aewozniak@gmail.com>
2017-01-21 12:44:03 -0500

    @sirexa can talk in binary!

--- slackbot_ce/matthew/slacklib.py
+++ slackbot_ce/matthew/slacklib.py
@@ -1,5 +1,6 @@
 # Put your commands here
 COMMAND1 = "who is mr_techie"
+COMMAND2 = "talk in binary"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -10,5 +11,7 @@ def handle_command(command):
     response = ""
     if command.find(COMMAND1) >= 0:
         response = "I cannot reveal his identity."
-        
+    if command.find(COMMAND2) >= 0:
+        response = "10010010 01010101 01010000 11111101 01010110 01010111 01011110 10101010 10101001\n00010111 11110101 01010010!!!"
+
     return response
