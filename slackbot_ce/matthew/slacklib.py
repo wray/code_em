7e54866e96dcc7a5de5188634f9a1e22650adc49
Matthew <aewozniak@gmail.com>
2017-01-21 13:07:05 -0500

    fixed responce <- see, spelled it wrong there too

--- slackbot_ce/matthew/slacklib.py
+++ slackbot_ce/matthew/slacklib.py
@@ -15,6 +15,6 @@ def handle_command(command):
     elif command.find(COMMAND2) >= 0:
         response = "10010010 01010101 01010000 11111101 01010110 01010111 01011110 10101010 10101001\n00010111 11110101 01010010!!!"
     elif command.find(COMMAND3) >= 0:
-        responce = "Well, I don't like you either!"
+        response = "Well, I don't like you either!"
 
     return response
