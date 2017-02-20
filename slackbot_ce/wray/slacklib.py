85b13fa201a19e4113af946a2543f12a151d2566
wray <wray@wrayesian.com>
2017-02-19 20:55:19 -0500

    debugging

--- slackbot_ce/wray/slacklib.py
+++ slackbot_ce/wray/slacklib.py
@@ -15,7 +15,9 @@ COMMAND5 = "get-ip"
 def mission_control(bot_id,output):
     for word in output['text'].split(" "):
         print(word)
+        led.red_led(1)
         if word.lower() == 'arm-1123':
+            led.red_led(0)
             led.green_led(1)
         elif word.lower() == 'disarm':
             led.green_led(0)
