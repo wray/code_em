201092b1ff424575b176b4f9ee7451c80fa965b0
joetechem <joe@techemstudios.com>
2017-11-07 13:48:20 -0500

    fixing student's slacklib syntax

--- slackbot_ce/code_em/alec/slacklib.py
+++ slackbot_ce/code_em/alec/slacklib.py
@@ -3,13 +3,13 @@ COMMAND1 = "hi"
 COMMAND2 = "error..."
 
 #Your handling code toes in this function
-def handle_command(comand):	
+def handle_command(command):	
 	
-    elif command.find(COMMAND1) >= 0:
+    if command.find(COMMAND1) >= 0:
         response = "Oh hi there, Please input ERROR..."
 
     elif command.find(COMMAND2) >= 0:
-       for i in range(100000000000000):
+       for i in range(5):
             response = "ERROR..."
         
     return response
