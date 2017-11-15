a82a62bdc2baf44e69ec594d6166c795f655a645
ZB <joe@techemstudios.com>
2017-11-14 19:24:06 -0500

    Command 2 for ZB

--- slackbot_ce/code_em/zb/slacklib.py
+++ slackbot_ce/code_em/zb/slacklib.py
@@ -1,5 +1,6 @@
 # Put your commands here
 COMMAND1 = "what is 4+4?"
+COMMAND2 = "does a fish live in water?"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -10,6 +11,10 @@ def handle_command(command):
     response = ""
     if COMMAND1 in command:
         response = "8"
+    
+    elif COMMAND2 in command:
+		response = "Yes."
+	
         
     return response
 
