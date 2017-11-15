137ff0e8344943e6b01c18ff3f7a8e3df8bb957a
coding777T <joe@techemstudios.com>
2017-11-14 20:03:03 -0500

    taixi

--- slackbot_ce/code_em/taixi/slacklib.py
+++ slackbot_ce/code_em/taixi/slacklib.py
@@ -1,15 +1,26 @@
 # Put your commands here
-COMMAND1 = "~~"
-
-# Your handling code goes in this function
+COMMAND1 = "placeholder"
+COMMAND2 = "what is your favorite food?"
+COMMAND3 = "why was 8 afraid of 7?"
+COMMAND4 = "what are your grades?"
+COMMAND5 = "can you change my grades?"
+COMMAND6 = "what is your favorite food?"
 def handle_command(command):
-    """
-        Determine if the command is valid. If so, take action and return
-        a response, if necessary.
-    """
-    response = ""
-    if COMMAND1 in command:
-        response = "Huh?"
-        
-    return response
+	response = ""
+	if COMMAND1 in command:
+		response = "a placeholder"
+	elif COMMAND2 in command:
+		response = "pizza"
+	elif COMMAND3 in command:
+		response = "!!!7,8(ate),9!!!"
+	elif COMMAND4 in command:
+		response = "Math: A+ , Science: A+ , Social Studies: F- , Reading: F- , Writing: F-"
+	elif COMMAND5 in command:
+		response = "Sure!  Math: A+ , Science: A+ , Social Studies: A+ , Reading: A+ , Writing: A+  "
+	elif COMMAND6 in command:
+		response = ""
+	else:
+		response = "Why thank you, I don't know what else to say."
+	return response
+
 
