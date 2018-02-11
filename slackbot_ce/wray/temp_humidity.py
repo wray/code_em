f11a21e48da04653f7b8ae6f7ef345e958417e48
Matthew <aewozniak@gmail.com>
2018-02-11 06:38:20 -0500

    Update temp_humidity.py

--- slackbot_ce/wray/temp_humidity.py
+++ slackbot_ce/wray/temp_humidity.py
@@ -14,12 +14,12 @@ pin = 4
 def read_temp_humidity():
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 	temp_f = temperature * 9.0 / 5.0 + 32.0
-	return int(temperature+.5), int(temp_f_.5), humidity
+	return int(temperature+.5), int(temp_f.5), humidity
 
 def get_temp():
 	humidity, temperature = Adafruit_DHT.read_retry(sensor,pin)
 	temp_f = temperature * 9.0/5.0+32
 
-	print temp_f
+	print int(temp_f+.5)
 	print humidity
-	return temp_f
+	return int(temp_f+.5)
