665b6fb16c31d58bf2f5baaad429fd8ae371323f
info@techemstudios.com <info@techemstudios.com>
2018-03-22 19:27:47 -0400

    Owen's Commands

--- slackbot_ce/code_em/owen/slacklib.py
+++ slackbot_ce/code_em/owen/slacklib.py
@@ -1,5 +1,9 @@
 # Put your commands here
-COMMAND1 = "~~"
+COMMAND1 = "is it iphone10 or iphonex"
+COMMAND2 = "do you like cake"
+COMMAND3 = "do you like the walking dead"
+COMMAND4 = "half-life 1 or half-life 2"
+COMMAND5 = "do you like me"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -9,6 +13,14 @@ def handle_command(command):
     """
     response = ""
     if COMMAND1 in command:
-        response = "Huh?"
+        response = "Iphone10."
+        response = " YEEEEEEEEEEEESSSSSSSSSSSSSSSSSS!" 
+        response = "The show stinks but the comic is geat."
+        response = "Half-life 1."
+        response = "No."
+        
+        
+        
+        
         
     return response
