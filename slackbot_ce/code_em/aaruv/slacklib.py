f5c114724534c3554b60fc588025d280da4ecc8c
info@techemstudios.com <info@techemstudios.com>
2018-03-22 19:26:10 -0400

    Aaruv's Commands

--- slackbot_ce/code_em/aaruv/slacklib.py
+++ slackbot_ce/code_em/aaruv/slacklib.py
@@ -1,14 +1,26 @@
 # Put your commands here
-COMMAND1 = "~~"
-
+COMMAND1 = "EPIC yo "
+COMMAND2 = "Hello"
+COMMAND3 = "what is your fav football team?"
+COMMAND4 = "Do you like mincraft?"
+COMMAND5 = "WHAT is your name"
 # Your handling code goes in this function
-def handle_command(command):
-    """
-        Determine if the command is valid. If so, take action and return
-        a response, if necessary.
-    """
-    response = ""
-    if COMMAND1 in command:
-        response = "Huh?"
-        
-    return response
+def handle_command(command):	
+	response = ""
+	if COMMAND1 in command:
+		response = "Huh?"
+
+	if COMMAND2 in command:
+		response = "you stink?"
+
+	if COMMAND3 in command:
+		response ="The FALCONS" 
+
+	if COMMAND4 in command:
+		response = " Yes" 
+
+	if COMMAND5 in command:
+		response = "EPic person"
+
+	return response
+    
