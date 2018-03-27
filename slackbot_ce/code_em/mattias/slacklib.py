8ebfe90a030ea542d093f64d11a4aaf9443ff5c8
info@techemstudios.com <info@techemstudios.com>
2018-03-27 19:00:21 -0400

    Mattias skills

--- slackbot_ce/code_em/mattias/slacklib.py
+++ slackbot_ce/code_em/mattias/slacklib.py
@@ -1,14 +1,24 @@
 # Put your commands here
-COMMAND1 = "~~"
-
+COMMAND1 = "URGRANNY"
+COMMAND2 = "URSISITER"
+COMMAND3 = "URGRANS"
+COMMAND4 = "URBROTHER"
+COMMAND5 = "URSISTER"
 # Your handling code goes in this function
 def handle_command(command):
     """
         Determine if the command is valid. If so, take action and return
-        a response, if necessary.
+        a respnse, if necessary.
     """
     response = ""
     if COMMAND1 in command:
-        response = "Huh?"
-        
+        response = "UR FAMILY TREE A FAT MONKEY" 
+    if COMMAND2 in command:
+        response = " UR BROTHER A MOTHER "
+	if COMMAND3 in command:
+		response = "UR SISTER A MISTER"
+    if COMMAND4 in command:
+        response = "UR DAUGHTER A FATHER"
+    if COMMAND5 in command:
+        response = "IF YOU SHIP MADDIE AND BOJANGLES YOU GE BODADDY"
     return response
