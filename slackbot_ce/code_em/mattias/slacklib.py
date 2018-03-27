043e0fc5343279288b0f38d371ae4402748742a9
info@techemstudios.com <info@techemstudios.com>
2018-03-27 19:10:32 -0400

    mattias changing uppercase cmds to lowercasegit add *!

--- slackbot_ce/code_em/mattias/slacklib.py
+++ slackbot_ce/code_em/mattias/slacklib.py
@@ -1,9 +1,9 @@
 # Put your commands here
-COMMAND1 = "URGRANNY"
-COMMAND2 = "URSISITER"
-COMMAND3 = "URGRANS"
-COMMAND4 = "URBROTHER"
-COMMAND5 = "URSISTER"
+COMMAND1 = "urgranny"
+COMMAND2 = "ursister"
+COMMAND3 = "urgrans"
+COMMAND4 = "urbrother"
+COMMAND5 = "ursistery"
 # Your handling code goes in this function
 def handle_command(command):
     """
@@ -12,13 +12,13 @@ def handle_command(command):
     """
     response = ""
     if COMMAND1 in command:
-        response = "UR FAMILY TREE A FAT MONKEY" 
+        response = "IF YOU SHIP MADDIE AND BOJANGLES YOU GET BODADDY" 
     if COMMAND2 in command:
-        response = " UR BROTHER A MOTHER "
+        response = "IF YOU SHIP MADDIE AND BOJANGLES YOU GET BODADDY"
 	if COMMAND3 in command:
-		response = "UR SISTER A MISTER"
+		response = "IF YOU SHIP MADDIE AND BOJANGLES YOU GET BODADDY"
     if COMMAND4 in command:
-        response = "UR DAUGHTER A FATHER"
+        response = "IF YOU SHIP MADDIE AND BOJANGLES YOU GET BODADDY"
     if COMMAND5 in command:
-        response = "IF YOU SHIP MADDIE AND BOJANGLES YOU GE BODADDY"
+        response = "IF YOU SHIP MADDIE AND BOJANGLES YOU GET BODADDY"
     return response
