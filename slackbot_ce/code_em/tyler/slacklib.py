06bcf42066b81c40ffc66b812385c340191cbabf
joetechem <joe@techemstudios.com>
2018-03-29 00:22:31 -0400

    indent/errors whitespaces

--- slackbot_ce/code_em/tyler/slacklib.py
+++ slackbot_ce/code_em/tyler/slacklib.py
@@ -7,18 +7,17 @@ COMMAND5 = "what is the weather"
 COMMAND6 = "why"
 # Your handling code goes in this function
 def handle_command(command):
-	response = ""
-	if COMMAND1 in command:
-		response = "is it me you're looking for"
-	if COMMAND2 in command:
-		response = "is dead"
-	if COMMAND3 in command:
-		response = "1898"
-	if COMMAND4 in command:
-		response = "k"
-	if COMMAND5 in command:
-		response = "floor going up on a Tuesday"
-
-	if COMMAND6 in command:
-		response = "are you single"
-	return response
+        response = ""
+        if COMMAND1 in command:
+                response = "is it me you're looking for"
+        if COMMAND2 in command:
+                response = "is dead"
+        if COMMAND3 in command:
+                response = "1898"
+        if COMMAND4 in command:
+                response = "k"
+        if COMMAND5 in command:
+                response = "floor going up on a Tuesday"
+        if COMMAND6 in command:
+                response = "are you single"
+        return response
